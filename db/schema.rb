8e052b8882a8beb568db1d8bbda570ab434050e9
akino102819 <k.akino1998@icloud.com>
2020-05-14 16:04:02 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_074004) do
-
-  create_table "lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_lists_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 2020_05_11_142955) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,5 +25,4 @@ ActiveRecord::Schema.define(version: 2020_05_13_074004) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "lists", "users"
 end
