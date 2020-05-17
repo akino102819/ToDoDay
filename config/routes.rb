2b125dc7b3ed844bce71a9982f798ef09dba15a9
akino102819 <k.akino1998@icloud.com>
2020-05-17 11:50:10 +0900

    card edit destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
   resources :lists, only: [:new, :create, :edit, :update, :destroy] do
-    resources :cards, only: [:new, :create]
+    resources :cards, except: [:index]
   end
     
 end
